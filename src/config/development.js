ec6c03096e149428b0147885290f3704077dffc3
Mario Brusarosco <mariobrusarosco@hotmail.com>
2019-07-31 23:55:05 -0300

    fix: Updating Dynamic Config

--- src/config/development.js
+++ src/config/development.js
@@ -1,7 +1,9 @@
 const devConfiguration = {
-  'configType': 'development',
-  'AccessControlAllowOrigin': '*',
-  // 'AccessControlAllowOrigin': 'dev-code-notes.herokuapp.com'
+  configType: 'development',
+  AccessControlAllowOrigin: '*',
+  // 'AccessControlAllowOrigin': 'dev-code-notes.herokuapp.com',
+  API_ROOT: 'http://localhost:9090',
+  GOOGLE_CALLBACK_URL: 'https://dev-code-notes.herokuapp.com/api/v1/auth/google/callback'
 }
 
 module.exports = devConfiguration
